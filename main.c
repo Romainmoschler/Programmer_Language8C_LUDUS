44047184adfe718df303e368541c61e577e41fcc
rmoschler <r.moschler@ludus-academie.fr>
2019-10-14 17:08:44 +0200

    Structure en C

--- main.c
+++ main.c
@@ -1,8 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#define NFOIS 5
+//Exemple de programme en C
 
 int main()
 {
-    printf("Hello world!\n");
+    int i;
+    float x;
+    float racx;
+
+
+    printf("Bonjour\n");
+    printf("calcul de %d racines carrees\n",NFOIS);
+
+    for(i=0;i<NFOIS;i++){
+        printf("Donnez un nombre : ");
+        scanf("%f",&x);
+        if(x<0.0)
+            printf("Le nombre %f ne possède pas de racine carree\n",x);
+        else{
+            racx=sqrt(x);
+            printf("le nombre %f a une racine carree: %f\n",x,racx);
+        }
+    }
+    printf("Travai Termine -Au revoir");
+
+
     return 0;
 }
